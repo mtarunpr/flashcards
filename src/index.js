58a891d61e7e59724ae56a9fa26b33d3c0c55fd6
mtarunpr <m.tarunprasad@gmail.com>
2020-06-19 10:34:31 +0530

    Redux + firebase

--- src/index.js
+++ src/index.js
@@ -2,4 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />,  document.getElementById('root'));
+import { BrowserRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import firebase from 'firebase/app';
+import { createStore, combineReducers } from 'redux';
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer
+} from 'react-redux-firebase';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbfEKrqqpXtIYLlkDbpC6RVDuvdoFw0hM",
+  authDomain: "bootcamp-tarun.firebaseapp.com",
+  databaseURL: "https://bootcamp-tarun.firebaseio.com",
+  projectId: "bootcamp-tarun",
+  storageBucket: "bootcamp-tarun.appspot.com",
+  messagingSenderId: "797485418099",
+  appId: "1:797485418099:web:570243ebd3d23dba784c06"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer
+  // firestore: firestoreReducer // <- needed if using firestore
+});
+
+// Create store with reducers and initial state
+const store = createStore(rootReducer, composeWithDevTools());
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users'
+  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+  // enableClaims: true // Get custom claims along with the profile
+}
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch
+  // createFirestoreInstance // <- needed if using firestore
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById('root'),
+);
